d177b49f21bf5cc9bd564d70da3e03b111186f6f
Bernadas Camille <camille.bernadas@etu.umontpellier.fr>
2022-09-09 20:33:47 +0200

    Basic functionality

---
+++ Runtime/DataPersistence/Data/GameData.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameData
+{
+   /*
+   * Edit to your liking.
+   * Everything must be serializable. Define and use classes for complex objects.
+   */
+    
+    public GameData(){
+    }
+    public void ClearLists() {
+        /*
+        * Add the lists here so they can be emptied.
+        * exampleListName.Clear();
+        */
+    }
+}
